1403682a24626e406888af5e40ea58151b376bd2
Niraj Limbu <nlimbu@yahoo.com>
2021-11-17 21:40:43 -0500

    password generator

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordCharacter ='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+~\\`|}{[]:;?><,./-=';
+
+var passwordValue = '';
+
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword() {  
   var passwordText = document.querySelector("#password");
+  var passwordLength = prompt("Choose your password length");
+  passwordValue = '';
 
-  passwordText.value = password;
+  for (var i = 0; i < passwordLength; i++) {
+    var number = Math.floor(Math.random() * passwordCharacter.length);
+    passwordValue += passwordCharacter.substring(number, number +1);
+  }
 
-}
+  if (passwordLength > 128 ) {
+    alert("Password length should be less than 128 characters");
+  } else {
+    passwordText.value = passwordValue;
+  }  
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
